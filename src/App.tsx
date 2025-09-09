9868524028a3e05bd3b1e0c2bfb109e07976a594
Donsaporo <jp14carbonell@gmail.com>
2025-09-09 17:48:57 -0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -110,7 +110,7 @@ function App() {
               OBZIDE
             </div>
             <div className="text-xl md:text-2xl lg:text-3xl font-medium text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-red-600">
-              Tech & Supply
+              PANAMÁ
             </div>
           </h1>
         </div>
